c889fde1da3e1ea0573a3cd7784b48dc6e5b98cc
yaboiwierdal <78000452+yaboiwierdal@users.noreply.github.com>
2021-05-12 16:31:01 -0400

    Update flashcard_create.py

--- flashcard_create.py
+++ flashcard_create.py
@@ -31,7 +31,7 @@ def create_flashcards(create_study_edit_exit):
             create_flashcards(take_input())
 
 
-    if create_study_edit_exit.lower() == "s":
+    elif create_study_edit_exit.lower() == "s":
         counter = 1
         cursor.execute("SELECT DISTINCT Deck FROM flashcards;")
         fetched_data = cursor.fetchall()
@@ -59,7 +59,64 @@ def create_flashcards(create_study_edit_exit):
         create_flashcards(take_input())
 
 
-    if create_study_edit_exit.lower() == "x":
+    elif create_study_edit_exit.lower() == "e":
+        counter = 1
+        cursor.execute("SELECT DISTINCT Deck FROM flashcards;")
+        fetched_data = cursor.fetchall()
+        for item in fetched_data:
+            print(f"{counter}. {item[0]}")
+            counter += 1
+        deck_to_edit = int(input("Which deck would you like to edit?\n"))
+        deck_to_edit = fetched_data[deck_to_edit - 1][0]
+        print(deck_to_edit)
+        cursor.execute(f"SELECT Front, Back FROM flashcards WHERE Deck='{deck_to_edit}'")
+        fetched_data = cursor.fetchall()
+        counter = 1
+        for item in fetched_data:
+            print(f"Card #{counter}: \n Front: {item[0]}\n Back: {item[1]}")
+            counter += 1
+        print("\n")
+        want_to_edit = True
+        while want_to_edit == True:
+            how_edit = input("Would you like to add cards, delete cards, change cards, delete the whole deck, or exit? (a/d/c/w/e)\n")
+            if how_edit == "a":
+                card_front = input("What do you want on the front of your card?\n")
+                card_back = input("What do you want on the back of your card?\n")
+                sql_command = f"""INSERT INTO flashcards VALUES ("{deck_to_edit}", "{card_front}", "{card_back}");"""
+                cursor.execute(sql_command)
+                print("Card changed")
+
+            elif how_edit == "d":
+                card_to_delete = int(input("What number card would you like to delete?\n"))
+                sql_command = f"""DELETE FROM flashcards WHERE Front="{fetched_data[card_to_delete - 1][0]}" AND Back="{fetched_data[card_to_delete - 1][1]}";"""
+                cursor.execute(sql_command)
+                print(fetched_data[card_to_delete - 1][0])
+                print(fetched_data[card_to_delete - 1][1])
+
+            elif how_edit == "c":
+                card_to_edit = int(input("What number card would you like to edit"))
+                card_front = input("What do you want on the front of the card?\n")
+                card_back = input("What doo you want on the back of the card?\n")
+                sql_command = f"""UPDATE flashcards SET Front="{card_front}", Back="{card_back}" WHERE Front="{fetched_data[card_to_edit - 1][0]}" AND Back="{fetched_data[card_to_edit - 1][0]}";"""
+                print("Card updated successfully.")
+            
+            elif how_edit == "w":
+                delete_certainty = input("Are you sure you want to delete this deck? (y/n)\n")
+                if delete_certainty == "y":
+                    sql_command = f"""DELETE FROM flashcards WHERE Deck="{deck_to_edit}";"""
+                    cursor.execute(sql_command)
+                else:
+                    return False
+
+            elif how_edit == "e":
+                return False
+
+            want_to_edit = input("Would you like to continue editing? (y/n)\n")
+            if want_to_edit == "n":
+                want_to_edit = False
+
+
+    elif create_study_edit_exit.lower() == "x":
         return None
 
 create_flashcards(take_input())
