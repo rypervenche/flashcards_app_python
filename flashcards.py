75cf0669a6f848e995b050957cfe7c224c111422
yaboiwierdal <78000452+yaboiwierdal@users.noreply.github.com>
2021-05-18 13:28:00 -0400

    Uploaded reformatted base

--- flashcards.py
+++ flashcards.py
@@ -6,154 +6,72 @@ connection = sqlite3.connect("/home/yaboiwierdal/Flashcards Python Project/pytho
 cursor = connection.cursor()
 
 
-def mode_select():
-    choice = input("Would you like to create a new deck, study a current deck, edit a deck,  or exit the program (create/study/edit/exit)? ")
-    if choice.lower() == "create":
-        create_flashcards()
-    elif choice.lower() == "study":
-        study_flashcards()
-    elif choice.lower() == "edit":
-        editing()
-    elif choice.lower() == "exit":
-        save_and_exit()
-    else:
-        print("That's not a proper command. Try again.")
-        mode_select()
+
+class Card:
+    def __init__(self, card_id, deck_id, front, back):
+        self.id = id
+        self.deck_id = deck_id
+        self.front = front
+        self.back = back
 
 
-
-def create_flashcards():
-    deck_name = input("What do you want to name your new deck? ")
-    check_duplicate = cursor.execute(f"SELECT Deck FROM flashcards WHERE Deck='{deck_name}'").fetchone()
-    if check_duplicate is not None:
-        taken_alert = input("That deck name is taken. Would you like to try another name (y/n)? ")
-        if taken_alert.lower() == "y":
-            create_flashcards()
-        else:
-            mode_select()
-    else:
-        creating = True
-        while creating == True:
-            card_front = input("What do you want on the front of this card? ")
-            card_back = input("What do you want on the back of this card? ")
-            confirmation = input(f"You are adding the following card to '{deck_name}':\n Front: {card_front}\n Back: {card_back}\nAre you sure you want to make this addition (y/n)")
-            if confirmation == "y":
-                cursor.execute(f"""INSERT INTO flashcards VALUES ("{deck_name}", "{card_front}", "{card_back}");""")
-                connection.commit()
-                continue_creating = input("Your addition has been saved. Would you like to add another card (y/n)?")
-            else:
-                continue_creating = input("Would you like to try again (y/n)?")
-            creating = False if continue_creating == "n" else True
-        mode_select()
+def create_db():
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS cards (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            deck_id INTEGER NOT NULL,
+            front TEXT NOT NULL,
+            back TEXT NOT NULL,
+            FOREIGN KEY(deck_id) REFERENCES decks(id),
+            UNIQUE (deck_id, front)
+        );"""
+    )
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS decks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL 
+        )"""
+    )
+    connection.commit()
 
 
-def get_flashcards():
-    decks = list_decks()
-    deck_to_edit = int(input("Which deck would you like to study? "))
-    deck_choice = decks[deck_to_edit - 1][0]
-    cursor.execute(f"SELECT Front, Back FROM flashcards WHERE Deck='{deck_choice}'")
-    flashcards = cursor.fetchall()
-    return flashcards
+def get_deck_id(deck_name):
+    cursor.execute("INSERT INTO decks (name) VALUES (?)", [deck_name])
+    cursor.execute("SELECT id FROM decks WHERE name = (?)", [deck_name])
+    return cursor.fetchone()[0]
 
 
-def study_flashcards():
-    flashcards = get_flashcards()
-    studying = True
-    while studying == True:
-        random.shuffle(flashcards)
-        for card_info_list in flashcards:
-            print(card_info_list[0])
-            try_again = True
-            while try_again == True:
-                guess = input("Input your guess: ")
-                if guess.lower() == card_info_list[1].lower():
-                    print("Correct!")
-                    try_again = False
-                else:
-                    continue_trying = input("Incorrect. Would you like to try again (y/n)? ")
-                    if continue_trying == "n":
-                        try_again = False
-        continue_studying = input("You have finished studying this deck. Would you like to study it again (y/n)? ")
-        if continue_studying == "n":
-            continue_studying = input("Would you like to study another deck or exit the studying program (study/exit)? ")
-            studying = False if continue_studying == "study" else True
-    mode_select()
+def update_db(deck_id, front, back):
+    cursor.execute("""INSERT INTO cards (deck_id, front, back) VALUES (?, ?, ?)""", (deck_id, front, back))
+    connection.commit()
 
 
-def edit_select():
-    edit_method = input("Would you like to add cards, delete cards, change cards, delete the whole deck, or exit (add/delete/change/delete deck/exit)? ")
-    return edit_method
+def get_cards(deck_id):
+    cards = dict()
+    for row in cursor.execute(f"SELECT cards.id, decks.name, front, back FROM cards INNER JOIN decks ON cards.deck_id = decks.id WHERE deck_id = (?)", [deck_id]):
+        card = f"{row[0]}, {row[1]}, {row[2]}, {row[3]}"
+        cards[row[2]] = card
+    return cards
 
 
-def list_decks():
-    cursor.execute("SELECT DISTINCT Deck FROM flashcards;")
-    decks = cursor.fetchall()
-    for counter, deck_name in enumerate(decks):
-        print(f" {counter + 1}. {deck_name[0]}")
-    return decks
+def main():
+    create_db()
+
+    chosen_deck = "this"
+    deck_id = get_deck_id(chosen_deck)
+
+    update_db(deck_id, "Доброе утро", "Good morning")
+    update_db(deck_id, "Добрый день", "Good afternoon")
+    update_db(deck_id, "Добрый вечер", "Good evening")
+
+    cards = get_cards(deck_id)
+
+    for card in cards:
+        print(card)
+        print(cards.get(card))
 
 
-def list_cards():
-    decks = list_decks()  
-    deck_to_edit = int(input("Which deck would you like to edit?\n"))
-    deck_choice = decks[deck_to_edit - 1][0]
-    cursor.execute(f"SELECT Front, Back FROM flashcards WHERE Deck='{deck_choice}'")
-    front_and_back = cursor.fetchall()
-    for counter, card in enumerate(front_and_back):
-        print(f"Card #{counter + 1}: \n Front: {card[0]}\n Back: {card[1]}")
-    return front_and_back, deck_choice
-
-
-def editing():
-    edit_method = edit_select()
-    front_and_back, deck_choice = list_cards()
-    editing = True
-    while editing == True:
-        if edit_method.lower() == "add":
-            card_front = input("What do you want on the front of this card? ")
-            card_back = input("What do you want on the back of this card? ")
-            confirmation = input(f"You are adding the following card to '{deck_choice}':\n Front: {card_front}\n Back: {card_back}\nAre you sure you want to make this addition (y/n)? ")
-            sql_command = f"""INSERT INTO flashcards VALUES ("{deck_choice}", "{card_front}", "{card_back}");"""
-            continue_confirmation = "Your addition has been saved. Would you like to add another card (y/n)? "
-
-        elif edit_method.lower() == "delete":
-            card_to_delete = int(input("What number card would you like to delete? "))
-            confirmation = input(f"You are deleting the following card from '{deck_choice}':\n Front: {front_and_back[card_to_delete - 1][0]}\n Back: {front_and_back[card_to_delete - 1][1]}\nAre you sure you want to make these changes (y/n)? ")
-            sql_command = f"""DELETE FROM flashcards WHERE Front="{front_and_back[card_to_delete - 1][0]}" AND Back="{front_and_back[card_to_delete - 1][1]}";"""
-            continue_confirmation = "Your deletion has been saved. Would you like to delete another card (y/n)? "
-
-        elif edit_method.lower() == "change":
-            card_to_change = int(input("What number card would you like to change? "))
-            card_front = input("What do you want on the front of the card?\n")
-            card_back = input("What doo you want on the back of the card?\n")
-            confirmation = input(f"You are requesting update this card in {deck_choice} to:\n Front: {card_front}\n Back: {card_back}\nAre you sure you want to make these changes (y/n)? ")
-            sql_command = f"""UPDATE flashcards SET Front="{front_and_back[card_to_change- 1][0]}", Back="{front_and_back[card_to_change - 1][1]}" WHERE Front="{front_and_back[card_to_change- 1][0]}" AND Back="{front_and_back[card_to_change - 1][1]}";"""
-            continue_confirmation = "Your change has been saved. Would you like to change another card in this deck (y/n)? "
-
-        elif edit_method.lower() == "delete deck":
-            decks = list_decks()
-            deck_to_edit = int(input("Which deck would you like to delete?\n"))
-            deck_choice = decks[deck_to_edit - 1][0]
-            confirmation = input(f"Are you sure you want to delete '{deck_choice}' (y/n)? ")
-            sql_command = f"""DELETE FROM flashcards WHERE Front="{front_and_back[card_to_delete - 1][0]}" AND Back="{front_and_back[card_to_delete - 1][1]}";"""
-            continue_confirmation = "Your deletion has been saved. Would you like to delete another deck? "
-
-
-        if confirmation == "y":
-            cursor.execute(sql_command)
-            connection.commit()
-            continue_editing = input(continue_confirmation)
-        else: 
-            continue_editing = input("Would you like to try again (y/n)? ")
-        editing = False if continue_editing == "n" else True
-    mode_select()
-
-
-def save_and_exit():
-    return False
-
-
-mode_select()
+main()
 
 connection.commit()
 
